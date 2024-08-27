4719b0b9c416a9e335d94ec1226bf0174a1d86f6
Jan Adam <adamhonzikcz@seznam.cz>
2024-08-27 02:22:51 +0200

    feat: created component for generating mesh

---
+++ website/src/components/WordMeshGenerator.js
@@ -0,0 +1,46 @@
+"use client"
+import React from 'react';
+import {Input} from "@nextui-org/input";
+
+const WordMeshGenerator = ({updateMeshRef}) => {
+  const [numOfRows, setNumOfRows] = React.useState(0);
+  const [numOfCols, setNumOfCols] = React.useState(0);
+  const update = () => {
+    updateMeshRef(numOfRows, numOfCols);
+  }
+  return (
+    <div className={"w-full justify-center flex"}>
+      <div className={"flex flex-col gap-2"}>
+        <div className={"flex flex-col"}>
+          <label htmlFor="numOfRows">Number of Rows</label>
+          <Input
+            className={"rounded text-black"}
+            id="numOfRows"
+            type="number"
+            value={numOfRows}
+            onValueChange={setNumOfRows}
+          />
+        </div>
+        <div className={"flex flex-col"}>
+          <label htmlFor="numOfCols">Number of Columns</label>
+          <Input
+            className={"text-black"}
+            id="numOfCols"
+            type="number"
+            value={numOfCols}
+            onValueChange={setNumOfCols}
+          />
+        </div>
+        <button
+          className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}
+          onClick={update}
+        >
+          Generate Word Mesh
+        </button>
+      </div>
+
+    </div>
+  );
+};
+
+export default WordMeshGenerator;
