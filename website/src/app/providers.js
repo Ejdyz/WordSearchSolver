9b1dd235463eb68db76349e741cccf57a2ff3be3
Jan Adam <adamhonzikcz@seznam.cz>
2024-08-27 02:23:52 +0200

    feat: implemented nextui provider

---
+++ website/src/app/providers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NextUIProvider} from "@nextui-org/react";
+
+const Providers = ({children}) => {
+  return (
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
+  );
+};
+
+export default Providers;
